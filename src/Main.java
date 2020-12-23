177ec1958688db72fb9dca8728f52b6dba72788f
gordonmhy <gh@hpri.33mail.com>
2020-12-23 17:17:28 +0800

    Equipped existing functionalities with multi-Squarelotron support

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,17 @@ public class Main {
     // Stores Squarelotron objects
     static ArrayList<Squarelotron> matrices = new ArrayList<>();
 
-    // Complemented by an ArrayList when multiple Squarelotrons exist
-    static Squarelotron current;
-    // Will become deprecated when multiple Squarelotrons exist
-    static int size;
-
     // Default input for the creation of Squarelotrons with
     // terms T(n)=n (n being natural numbers and <= size^2) integer items
     private static final int[] EMPTY = new int[0];
 
     public static void main(String[] args) {
+        int id;
+        int ring;
+        int size;
         System.out.println("==== SQUARELOTRON ====");
         System.out.println("Start by creating a matrix (SETMATRIX)");
         printHelp();
-        int id;
-        int ring;
         String filename;
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -31,9 +27,9 @@ public class Main {
                 case "ADDMATRIX":
                     size = Integer.parseInt(scanner.next());
                     filename = scanner.next();
-                    Squarelotron matrix = new Squarelotron(size, filename.equals("default") ? EMPTY : importFromFile(filename));
+                    Squarelotron matrix = new Squarelotron(size, filename.equals("default") ? EMPTY : importFromFile(filename, size));
                     matrices.add(matrix);
-                    System.out.println("Matrix added.\nVisualization:");
+                    System.out.println("Squarelotron (ID: " + matrix.getId() + ") added.\nVisualization:");
                     matrix.printMatrix();
                     break;
                 case "HELP":
@@ -50,20 +46,22 @@ public class Main {
                     }
                     if (i % 5 != 0)
                         System.out.println();
+                    System.out.println("Count: " + ((Integer) i).toString());
                     break;
                 case "MAINDIAGONALFLIP":
-                    if (current == null){
-                        System.out.println("You have not set a matrix.");
+                    id = Integer.parseInt(scanner.next());
+                    if (!matrixExists(id)) {
+                        System.out.println("Invalid ID.");
                         continue;
                     }
                     ring = Integer.parseInt(scanner.next());
-                    if (ring < 1 || ring > Math.ceil(size / (double) 2)){
+                    if (ring < 1 || ring > Math.ceil(getMatrix(id).getSize() / (double) 2)){
                         System.out.println("Invalid ring.");
                         continue;
                     }
-                    current.mainDiagonalFlip(ring);
-                    System.out.println("Matrix flipped.\nVisualization:");
-                    current.printMatrix();
+                    getMatrix(id).mainDiagonalFlip(ring);
+                    System.out.println("Squarelotron (ID: " + id + ") flipped.\nVisualization:");
+                    getMatrix(id).printMatrix();
                     break;
                 case "PRINT":
                     id = Integer.parseInt(scanner.next());
@@ -71,7 +69,7 @@ public class Main {
                         System.out.println("Invalid ID.");
                         continue;
                     }
-                    System.out.println("Matrix (ID: " + id + ")\nVisualization:");
+                    System.out.println("Squarelotron (ID: " + id + ")\nVisualization:");
                     getMatrix(id).printMatrix();
                     break;
                 case "QUIT":
@@ -83,48 +81,43 @@ public class Main {
                         continue;
                     }
                     matrices.remove(getMatrix(id));
-                    System.out.println("Matrix (ID: " + id + ") removed.");
+                    System.out.println("Squarelotron (ID: " + id + ") removed.");
                     break;
                 case "ROTATE":
-                    if (current == null){
-                        System.out.println("You have not set a matrix.");
+                    id = Integer.parseInt(scanner.next());
+                    if (!matrixExists(id)) {
+                        System.out.println("Invalid ID.");
                         continue;
                     }
                     int n = Integer.parseInt(scanner.next());
-                    current.rotateRight(n);
-                    System.out.println("Matrix rotated.\nVisualization:");
-                    current.printMatrix();
-                    break;
-                // Will be replaced by ADDMATRIX when multimatrix support is implemented
-                case "SETMATRIX":
-                    size = Integer.parseInt(scanner.next());
-                    filename = scanner.next();
-                    current = new Squarelotron(size, filename.equals("default") ? EMPTY : importFromFile(filename));
-                    System.out.println("Matrix set.\nVisualization:");
-                    current.printMatrix();
+                    getMatrix(id).rotateRight(n);
+                    System.out.println("Squarelotron rotated.\nVisualization:");
+                    getMatrix(id).printMatrix();
                     break;
                 case "TRANSPOSE":
-                    if (current == null){
-                        System.out.println("You have not set a matrix.");
+                    id = Integer.parseInt(scanner.next());
+                    if (!matrixExists(id)) {
+                        System.out.println("Invalid ID.");
                         continue;
                     }
-                    current.transpose();
-                    System.out.println("Matrix transposed.\nVisualization:");
-                    current.printMatrix();
+                    getMatrix(id).transpose();
+                    System.out.println("Squarelotron (ID: " + id + ") transposed.\nVisualization:");
+                    getMatrix(id).printMatrix();
                     break;
                 case "UPSIDEDOWNFLIP":
-                    if (current == null){
-                        System.out.println("You have not set a matrix.");
+                    id = Integer.parseInt(scanner.next());
+                    if (!matrixExists(id)) {
+                        System.out.println("Invalid ID.");
                         continue;
                     }
                     ring = Integer.parseInt(scanner.next());
-                    if (ring < 1 || ring > Math.ceil(size / (double) 2)){
+                    if (ring < 1 || ring > Math.ceil(getMatrix(id).getSize() / (double) 2)){
                         System.out.println("Invalid ring.");
                         continue;
                     }
-                    current.upsideDownFlip(ring);
-                    System.out.println("Matrix flipped.\nVisualization:");
-                    current.printMatrix();
+                    getMatrix(id).upsideDownFlip(ring);
+                    System.out.println("Squarelotron (ID: " + id + ") flipped.\nVisualization:");
+                    getMatrix(id).printMatrix();
                     break;
             }
         }
@@ -162,7 +155,7 @@ public class Main {
      * @param filename Name of file that contains a single squarelotron
      * @return int[] Contents of the Squarelotron
      */
-    private static int[] importFromFile(String filename) {
+    private static int[] importFromFile(String filename, int size) {
         int[] result = new int[size * size];
         try {
             File file = new File(filename);
@@ -186,13 +179,16 @@ public class Main {
 
     private static void printHelp() {
         System.out.println("--- List of Commands ---");
+        System.out.println("- ADDMATRIX <size> <filename|'default'>");
         System.out.println("- HELP");
-        System.out.println("- MAINDIAGONALFLIP <ring(Between 1 and CEIL(size/2))>");
+        System.out.println("- LIST");
+        System.out.println("- MAINDIAGONALFLIP <ID> <ring(Between 1 and CEIL(size/2))>");
+        System.out.println("- PRINT <ID>");
         System.out.println("- QUIT");
-        System.out.println("- ROTATE <turns(Any Real Number)>");
-        System.out.println("- SETMATRIX <size(Any Positive Number)>");
-        System.out.println("- TRANSPOSE");
-        System.out.println("- UPSIDEDOWNFLIP <ring(Between 1 and CEIL(size/2))>");
+        System.out.println("- REMOVEMATRIX <ID>");
+        System.out.println("- ROTATE <ID> <turns(Any Real Number)>");
+        System.out.println("- TRANSPOSE <ID>");
+        System.out.println("- UPSIDEDOWNFLIP <ID> <ring(Between 1 and CEIL(size/2))>");
         System.out.println();
     }
 
